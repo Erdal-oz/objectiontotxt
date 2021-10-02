f4e4d0d48705a67d79face27d123d4f8bbf688f1
Plright <plright1@protonmail.com>
2021-10-02 17:43:28 +0300

    master

--- setup.py
+++ setup.py
@@ -1,24 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-VERSION = '0.0.1' 
-DESCRIPTION = 'Objectionlol to Txt'
-LONG_DESCRIPTION = 'This package is made for the purpose of making objection.lol\'s technical stuff easy'
-
-setup(
-        name="objectionloltotxt", 
-        version=VERSION,
-        author="Plright",
-        author_email="plright1@protonmail.com",
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        packages=find_packages(),
-        install_requires=[],
-        keywords=['python', 'objectionlol to txt'],
-        classifiers= [
-            "Development Status :: 3 Alpha",
-            "Intended Audience :: Objection.lol Users",
-            "Programming Language :: Python :: 3",
-            "Operating System :: Microsoft :: Windows",
-            "Operating System :: Linux",
-        ]
-)
+if __name__ == "__main__":
+    setup()
