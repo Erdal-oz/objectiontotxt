e0bf5456538c522c1122055ab9f5401696400575
Plright <plright1@protonmail.com>
2021-10-16 13:38:55 +0300

    Deleted the bin/objtotxt script since the same functionality is provided with entry point in setup.cfg
    -d argument now accepts multiple directories

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ A really really simple program to convert objection files from https://objection
 I am not experienced with creating projects or programming stuff for public use so if there are any problems i would appreciate feedback.
 
 
+## Usage
+
+This program is used as a commandline program.
+
+For giving filenames of the .objection files
+```
+$ objtoxt --filename file1 file2 file3...
+```
+
+For giving the directory where the .objection files are at
+```
+$ objtotxt -d directory1 directory2...
+```
--- src/objectionloltotxt/__main__.py
+++ src/objectionloltotxt/__main__.py
@@ -10,31 +10,38 @@ def main():
     mutually_exclusive_arguments = commandline_interface.add_mutually_exclusive_group(required=True)
     mutually_exclusive_arguments.add_argument('--filename', metavar="file", type=str, nargs='+',
                                               help="Filename of the .objection file")
-    mutually_exclusive_arguments.add_argument('-d', metavar="directory", type=str,
+    mutually_exclusive_arguments.add_argument('-d', metavar="directory", type=str, nargs='+',
                                               help='convert the .objection files in a directory to readable text file')
     args = commandline_interface.parse_args()
     input_filename = args.filename
     input_directory = args.d
-    if input_filename is not None:
+
+    if input_directory is not None: # if -d argument is used instead of --filename the code after the if condition is executed, since they are mutually exclusive
+        for directory in input_directory:
+            if str(directory) is not None and not os.path.isdir(str(directory)):
+                print(f"{directory} directory doesn't exist")
+                input_directory.remove(directory) 
+                assert input_directory != (), "all the directories you provided are invalid"
+
+        for folder in input_directory:
+            list_of_file = os.listdir(folder)
+            for file in list_of_file:
+                full_path = os.path.join(folder, file)
+                if file.endswith(".objection"):
+                    objectiontotxt.convert_base64objection_to_readable_text_file(full_path)
+                else:
+                    print(f"please rename {full_path} to have .objection extension if the file is an objection file")
+
+
+    else: 
         for file in input_filename:
             if not os.path.isfile(file):
                 print("That file doesn't exist")
                 sys.exit()
-
-    if input_directory is not None and not os.path.isdir(input_directory):
-        print("that directory doesn't exist")
-        sys.exit()
-    if input_directory is not None:
-        list_of_file = os.listdir(input_directory)
-        for file in list_of_file:
-            if file.endswith(".objection"):
-                full_path = os.path.join(input_directory, file)
-                objectiontotxt.convert_base64objection_to_readable_text_file(full_path)
-
-    else:
-        for file in input_filename:
-            if file.endswith(".objection"):
+            elif file.endswith(".objection"):
                 objectiontotxt.convert_base64objection_to_readable_text_file(file)
+            else:
+                print(f"please rename \"{file}\" to have .objection extension if the file is an objection file ")
 
 if __name__ == "__main__":
     main()
